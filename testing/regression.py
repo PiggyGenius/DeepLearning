16e67080bba96eeee646bedb5c155aee2ae8d508
PiggyGenius <ludo.itrts@gmail.com>
2017-10-02 13:27:40 +0000

    Add analysis on tensorflow functionalities and a tensorboard example

--- testing/regression.py
+++ testing/regression.py
@@ -2,6 +2,14 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+def make_random_data():
+    x = np.random.uniform(low=-2, high=4, size=200)
+    y = []
+    for t in x:
+        r = np.random.normal(loc=0.0, scale=(0.5 + t*t/3), size=None)
+        y.append(r)
+    return x, 1.726*x - 0.84 + np.array(y)
+
 # linear regression mode: ŷ = w * x + b
 # w and b are variables, x is the input which we can define as a placeholder
 # We use the Mean Squared Error to formulate a cost function where y is a placeholder as well
@@ -21,15 +29,7 @@ with g.as_default():
     cost = tf.reduce_mean(tf.square(tf_y - y_hat), name='cost')
     optim = tf.train.GradientDescentOptimizer(learning_rate=0.001)
     train_op = optim.minimize(cost, name='train_op')
-
 np.random.seed(0)
-def make_random_data():
-    x = np.random.uniform(low=-2, high=4, size=200)
-    y = []
-    for t in x:
-        r = np.random.normal(loc=0.0, scale=(0.5 + t*t/3), size=None)
-        y.append(r)
-    return x, 1.726*x - 0.84 + np.array(y)
 x, y = make_random_data()
 plt.plot(x, y, 'o')
 plt.show()
@@ -47,4 +47,52 @@ with tf.Session(graph=g) as sess:
         if not e % 50:
             print('Epoch {}: {}'.format(e, c))
 plt.plot(training_costs)
+plt.show()
+
+# We may develop a model in a separate module, variables are not available in a different scope
+# If we have a graph, we can execute the nodes of the graph using their names in the graph
+# Tensorflow appends ':0' to the name of the first tensor, '_1:0' to the second and so on...
+n_epochs = 500
+training_costs = []
+with tf.Session(graph=g) as sess:
+    sess.run(tf.global_variables_initializer())
+    for e in range(n_epochs):
+        c, _ = sess.run(['cost: 0', 'train_op'], feed_dict={'tf_x:0': x_train, 'tf_y:0': y_train})
+        training_costs.append(c)
+        if e % 50 == 0:
+            print('Epoch {:4d}: {:.4f}'.format(e, c))
+
+# When the execution of the statements are finished and we exit the tf.Session, memory is freed
+# To make the prediction on our test set, we save the model with a new node: tf.train.Saver
+with g.as_default():
+    saver = tf.train.Saver()
+n_epochs = 500
+training_costs = []
+with tf.Session(graph=g) as sess:
+    sess.run(tf.global_variables_initializer())
+    for e in range(n_epochs):
+        c, _ = sess.run([cost, train_op], feed_dict={tf_x: x_train, tf_y: y_train})
+        training_costs.append(c)
+        if not e % 50:
+            print('Epoch {}: {}'.format(e, c))
+    saver.save(sess, './trained-model')
+# To restore a trained model we rebuild the graph who has the same nodes and names as the saved model
+# Then we restore the saved variables in a new tf.Session
+# We rebuild the graph by importing it from the meta file
+g2 = tf.Graph()
+with tf.Session(graph=g2) as sess:
+    new_saver = tf.train.import_meta_graph('./trained-model.meta')
+    new_saver.restore(sess, './trained-model')
+    y_pred = sess.run('y_hat:0', feed_dict={'tf_x:0': x_test})
+
+x_arr = np.arange(-2, 4, 0.1)
+g2 = tf.Graph()
+with tf.Session(graph=g2) as sess:
+    new_saver = tf.train.import_meta_graph('./trained-model.meta')
+    new_saver.restore(sess, './trained-model')
+    y_arr = sess.run('y_hat:0', feed_dict={'tf_x:0': x_arr})
+plt.figure()
+plt.plot(x_train, y_train, 'bo')
+plt.plot(x_test, y_test, 'bo', alpha=0.3)
+plt.plot(x_arr, y_arr.T[:, 0], '-r', lw=3)
 plt.show()
--- testing/tensorLearn2.py
+++ testing/tensorLearn2.py
@@ -1,20 +1,101 @@
 import tensorflow as tf
 import numpy as np
 
-# Executing variables and operators by their names is very useful in many scenarios. For example, we may develop a model in a separate module; and thus the variables are not available in a different Python scope according to Python scoping rules. However, if we have a graph, we can execute the nodes of the graph using their names in the graph.
-# This can be done easily by changing the sess.run method from the previous code example, using the variable name of the cost in the graph rather than the Python variable cost by changing sess.run([cost, train_op], ...) to sess.run(['cost:0', 'train_op'], ...)
-# Tensorflow appends ':0' to the name of the first tensor, '_1:0' to the second and so on...
-n_epochs = 500
-training_costs = []
+##################################################################################
+################ We can transform tensors as multidimensional array ##############
+##################################################################################
+
+g = tf.Graph()
+with g.as_default():
+    arr = np.array([[1., 2., 3., 3.5], [4., 5., 6., 6.5], [7., 8., 9., 9.5]])
+    T1 = tf.constant(arr, name='T1')
+    s = T1.get_shape()
+    T2 = tf.Variable(tf.random_normal(shape=s))
+    T3 = tf.Variable(tf.random_normal(shape=(s.as_list()[0], )))
+    print(T1)
+    print(T2)
+    print(T3)
+    print('Shape of T1 is', s)
+# We can reshape tensors
+# -1 means that the size will be infered based on the size of the array and the specified dimensions
+with g.as_default():
+    T4 = tf.reshape(T1, shape=[1, 1, -1], name='T4')
+    T5 = tf.reshape(T1, shape=[1, 3, -1], name='T5')
+    print(T4)
+    print(T5)
 with tf.Session(graph=g) as sess:
-    sess.run(tf.global_variables_initializer())
-    for e in range(n_epochs):
-        c, _ = sess.run(['cost: 0', 'train_op'], feed_dict={'tf_x:0': x_train, 'tf_y:0': y_train})
-        training_costs.append(c)
-        if e % 50 == 0:
-            print('Epoch {:4d}: {:.4f}'.format(e, c))
+    print(sess.run(T4))
+    print(sess.run(T5))
+# We can transpose and change the order of dimensions with perm
+with g.as_default():
+    T6 = tf.transpose(T5, perm=[2, 1, 0], name='T6')
+    T7 = tf.transpose(T5, perm=[0, 2, 1], name='T7')
+    print(T6)
+    print(T7)
+# We can split a tensor into a list of subtensors
+with g.as_default():
+    t5_splt = tf.split(T5, num_or_size_splits=2, axis=2, name='T8')
+    print(t5_splt)
+# We can combine a list of tensors with the same shape and dtype
+g = tf.Graph()
+with g.as_default():
+    t1 = tf.ones(shape=(5, 1), dtype=tf.float32, name='t1')
+    t2 = tf.zeros(shape=(5, 1), dtype=tf.float32, name='t2')
+    print(t1)
+    print(t2)
+with g.as_default():
+    t3 = tf.concat([t1, t2], axis=0, name='t3')
+    t4 = tf.concat([t1, t2], axis=1, name='t4')
+    print(t3)
+    print(t4)
+with tf.Session(graph=g) as sess:
+    print(t3.eval())
+    print(t4.eval())
 
 
-# In the previous section, we built a graph and trained it. How about doing the actual prediction on the held out test set? The problem is that we did not save the model parameters; so, once the execution of the preceding statements are finished and we exit the tf.Session environment, all the variables and their allocated memories are freed.
-# One solution is to train a model, and as soon as the training is finished, we can feed it our test set. However, this is not a good approach since deep neural network models are typically trained over multiple hours, days, or even weeks.
-# The best approach is to save the trained model for future use
+##################################################################################
+######################## Control flow mechanics in graphs ########################
+##################################################################################
+
+# We implement this equation in Tensorflow: res = x + y if x < y and x - y otherwise
+# This is incorrect, the computation graph has only one branch associated with the addition operator
+# The computation graph is static, once the graph is built it remains unchanged during the execution
+# Even when we change the values, the new tensors will go through the same path in the graph
+x, y = 1.0, 2.0
+g = tf.Graph()
+with g.as_default():
+    tf_x = tf.placeholder(dtype=tf.float32, shape=None, name='tf_x')
+    tf_y = tf.placeholder(dtype=tf.float32, shape=None, name='tf_y')
+    if x < y:
+        res = tf.add(tf_x, tf_y, name='result_add')
+    else:
+        res = tf.substract(tf_x, tf_y, name='result_sub')
+    print('Object:', res)
+with tf.Session(graph=g) as sess:
+    print('x < y: {} -> Result:'.format(x < y), res.eval(feed_dict={'tf_x:0': x, 'tf_y:0': y}))
+    x, y = 2.0, 1.0
+    print('x < y: {} -> Result:'.format(x < y), res.eval(feed_dict={'tf_x:0': x, 'tf_y:0': y}))
+# Now let's implement it correctly using control flow mechanics in TensorFlow
+x, y = 1.0, 2.0
+g = tf.Graph()
+with g.as_default():
+    tf_x = tf.placeholder(dtype=tf.float32, shape=None, name='tf_x')
+    tf_y = tf.placeholder(dtype=tf.float32, shape=None, name='tf_y')
+    res = tf.cond(tf_x < tf_y, 
+            lambda: tf.add(tf_x, tf_y, name='result_add'), 
+            lambda: tf.subtract(tf_x, tf_y, name='result_sub'))
+    print('Object:', res)
+with tf.Session(graph=g) as sess:
+    print('x < y: {} -> Result:'.format(x < y), res.eval(feed_dict={'tf_x:0': x, 'tf_y:0': y}))
+    x, y = 2.0, 1.0
+    print('x < y: {} -> Result:'.format(x < y), res.eval(feed_dict={'tf_x:0': x, 'tf_y:0': y}))
+# To implement this statement: if (x < y) result = 1 else result = 0
+f1 = lambda: tf.constant(1)
+f2 = lambda: tf.constant(0)
+result = tf.case([(tf.less(x, y), f1)], default=f2)
+# We can also do a while loop
+i = tf.constant(0)
+threshold = 100
+c = lambda i: tf.less(i, 100)
+b = lambda i: tf.add(i, 1)
+r = tf.while_loop(cond=c, body=b, loop_vars=[i])
